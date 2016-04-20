2e10d973e32cf9b37c728a8441929681c4b19a03
kakwa <kakwa@laphroaig.kakwa.fr>
2016-04-20 08:04:22 +0200

    disable special layouts

--- config.h
+++ config.h
@@ -27,8 +27,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            0,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "false",     NULL,       NULL,       0,            0,           -1 }, // must keep one fake rule
 };
 
 /* layout(s) */
