61f855b3469d541cdfeac1e9837a6a4a03d97cee
carpenti <carpenti@adobe.com>
2019-11-27 15:31:49 +0100

    update to dwm 6.2

--- config.h
+++ config.h
@@ -18,6 +18,13 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
+static const char *colors[][3]      = {
+        /*               fg         bg         border   */
+        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+        [SchemeSel] = { selfgcolor, selbgcolor, selbordercolor },
+};
+
+
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
